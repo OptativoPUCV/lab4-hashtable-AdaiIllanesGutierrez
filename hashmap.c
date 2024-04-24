a366e8702421b10ad27310247b77a291bb3f7a9d
AdaiIllanesGutierrez <112001617+AdaiIllanesGutierrez@users.noreply.github.com>
2024-04-24 21:16:09 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,9 +82,11 @@ HashMap * createMap(long capacity) {
 void eraseMap(HashMap * map,  char * key) {    
 Pair *pair=searchMap(map, key);
   if(pair!=NULL){
+    free(pair->key);
+    free(pair->value);
+    free(pair);
     map->buckets[map->current]=NULL;
     map->size --;
-    pair->key=NULL;
   }
   
 }
