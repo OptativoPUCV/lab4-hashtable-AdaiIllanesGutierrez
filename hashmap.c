c5fab7ac7737f180796a761b205d64a7976a9636
AdaiIllanesGutierrez <112001617+AdaiIllanesGutierrez@users.noreply.github.com>
2024-04-24 21:37:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,9 +82,6 @@ HashMap * createMap(long capacity) {
 void eraseMap(HashMap * map,  char * key) {    
   Pair *pair = searchMap(map, key);
   if (pair != NULL) {
-      /*free(pair->key);
-      free(pair->value);
-      free(pair);*/
       map->buckets[map->current]->key= NULL;
       map->size--; 
   }
@@ -114,7 +111,7 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
-  for (long i = map->current ; i < map->capacity; i++) {
+  for (long i = map->current + 1; i < map->capacity; i++) {
     if (map->buckets[i] != NULL) {
         map->current = i;
         return map->buckets[i];
